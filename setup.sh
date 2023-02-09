731313067d692e8fefc95786f102ceb182afa509
yuasalily <yuasalily1011@gmail.com>
2023-02-10 02:59:32 +0900

    vscode用設定追加

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 apt update
 apt install -y curl gcc
 
+mkdir .vscode
+curl -L -o .vscode/settings.json https://raw.githubusercontent.com/yuasalily/setup/main/settings.json
+
 if [ $1 = "rust" ]; then
     bash -c "$(curl -L https://raw.githubusercontent.com/yuasalily/setup/main/setup_rust.sh)"
 fi
