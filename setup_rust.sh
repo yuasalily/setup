61dbe6bbb8e828ba899b8fe1ad88c27e7d08642a
yuasalily <yuasalily1011@gmail.com>
2023-02-11 02:26:20 +0900

    rust install修正

--- setup_rust.sh
+++ setup_rust.sh
@@ -1,4 +1,4 @@
-curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh -s -- -y
 source "$HOME/.cargo/env"
 rustup component add rustfmt
 rustup component add clippy
