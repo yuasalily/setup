50b4b77f023d32e8aee1b9399469a56f7db85e18
yuasalily <yuasalily1011@gmail.com>
2023-02-11 17:46:23 +0900

    keybindings削除

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@ apt install -y gcc
 
 mkdir -p .vscode
 curl -L -o .vscode/settings.json https://raw.githubusercontent.com/yuasalily/setup/main/.vscode/settings.json
-curl -L -o .vscode/keybindings.json https://raw.githubusercontent.com/yuasalily/setup/main/.vscode/keybindings.json
 curl -L -o .vscode/extensions.json https://raw.githubusercontent.com/yuasalily/setup/main/.vscode/extensions.json
 
 if [ $1 = "rust" ]; then
