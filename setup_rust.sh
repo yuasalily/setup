feae2ec2e90820d918c4e3ab82b2ec0bcf55ae34
yuasalily <yuasalily1011@gmail.com>
2023-02-17 00:10:03 +0900

    extentions修正

--- setup_rust.sh
+++ setup_rust.sh
@@ -1,3 +1,5 @@
+curl -L -o .vscode/extensions.json https://raw.githubusercontent.com/yuasalily/setup/main/.vscode/extensions-rust.json
+
 curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh -s -- -y
 source "$HOME/.cargo/env"
 rustup component add rustfmt
