8dc3218f3dd5add4ae0c9fb2d882f344ef426cdb
yuasalily <yuasalily1011@gmail.com>
2023-02-17 00:12:02 +0900

    削除設定追加

--- setup_go.sh
+++ setup_go.sh
@@ -2,4 +2,5 @@ curl -L -o .vscode/extensions.json https://raw.githubusercontent.com/yuasalily/s
 
 curl -O https://dl.google.com/go/go1.20.1.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.20.1.linux-amd64.tar.gz
+rm -rf go1.20.1.linux-amd64.tar.gz
 echo "export PATH=$PATH:/usr/local/go/bin" >> $HOME/.profile
