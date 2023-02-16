98295312c1b0fee469c097257a6e95efe592cbc9
yuasalily <yuasalily1011@gmail.com>
2023-02-17 00:10:18 +0900

    extensions修正

--- setup.sh
+++ setup.sh
@@ -2,8 +2,11 @@ apt install -y gcc
 
 mkdir -p .vscode
 curl -L -o .vscode/settings.json https://raw.githubusercontent.com/yuasalily/setup/main/.vscode/settings.json
-curl -L -o .vscode/extensions.json https://raw.githubusercontent.com/yuasalily/setup/main/.vscode/extensions.json
 
 if [ $1 = "rust" ]; then
     bash -c "$(curl -L https://raw.githubusercontent.com/yuasalily/setup/main/setup_rust.sh)"
+fi
+
+if [ $1 = "go" ]; then
+    bash -c "$(curl -L https://raw.githubusercontent.com/yuasalily/setup/main/setup_go.sh)"
 fi
